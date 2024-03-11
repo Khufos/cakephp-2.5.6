7dc5fb29a67bee9d205e94314cab3c8436cf5539
khufos <iagojosephqt@gmail.com>
2024-03-11 01:32:42 -0300

    orderr

--- app/Controller/FilmesController.php
+++ app/Controller/FilmesController.php
@@ -11,8 +11,8 @@ class FilmesController extends AppController
         //     array('Filme' => array('nome' => 'Pantera ', 'ano' => '2020', 'duracao' => '10.0', 'idioma' => 'Português')),
         //     array('Filme' => array('nome' => 'Sapoto', 'ano' => '2045', 'duracao' => '2.0', 'idioma' => 'Italiano')),
         // );
-
-        $filmes = $this->Filme->find('all');
+        $order = array('Filme.nome'=> 'asc');
+        $filmes = $this->Filme->find('all',compact('order'));
         $this->set(compact('filmes'));
     }
 }
